4a89112a8990b25e01538d906470792721ff2418
John Rykken <john.rykken@gmail.com>
2018-03-02 10:02:18 -0800

    Add functional get routes for every page. Add add-brand/add-store.

--- app.rb
+++ app.rb
@@ -7,6 +7,43 @@ require("./lib/brand")
 require("./lib/store")
 require("pry")
 
-get('/')do
+get('/') do
   erb(:home)
+end
+
+get('/home') do
+  erb(:home)
+end
+
+get('/brand') do
+  @brands = Brand.all
+  erb(:brand)
+end
+
+get('/store') do
+  @stores = Store.all
+  erb(:store)
+end
+
+post('/store') do
+  name = params[:name]
+  Store.create({:name => name})
+  redirect("/store")
+end
+
+post('/brand') do
+  title = params[:title]
+  price = params[:price]
+  Brand.create({:title => title, :price => price})
+  redirect("/brand")
+end
+
+get('/store/:id') do
+  @store = Store.find(params.fetch("id").to_i())
+  erb(:store_edit)
+end
+
+get('/brand/:id') do
+  @brand = Brand.find(params.fetch("id").to_i())
+  erb(:brand_edit)
 end
